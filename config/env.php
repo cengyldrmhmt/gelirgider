f65eb8172dc331117b32380b5942807567fb8809
cengyldrmhmt <122272954+cengyldrmhmt@users.noreply.github.com>
2025-05-28 18:08:52 +0300

    Add files via upload

---
+++ config/env.php
@@ -0,0 +1,11 @@
+<?php
+// Ortam değişkenleri için örnek yapı
+return [
+    'DB_HOST' => 'localhost',
+    'DB_NAME' => 'gelirgider',
+    'DB_USER' => 'root',
+    'DB_PASS' => '',
+    'APP_ENV' => 'local',
+    'APP_DEBUG' => true,
+    'APP_URL' => 'http://localhost/gelirgider/public',
+]; 
